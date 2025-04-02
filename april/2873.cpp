e6698e4bd467e4e44c9d8c4f57036d4f7b5bea16
cmd-anurag <anuragkumar.7157@gmail.com>
2025-04-02 13:44:47 +0530

    2873

--- april/2873.cpp
+++ april/2873.cpp
@@ -1,28 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-
-class Solution {
-    public:
-        long long maximumTripletValue(vector<int>& nums) {
-            long long int result = 0;
-            int n = nums.size();
-            for(int i = 0; i < n; ++i) {
-                for(int j = i+1; j < n; ++j) {
-                    for(int k = j+1; k < n; ++k) {
-                        long long int points = (long long)(nums[i] - nums[j]) * nums[k];
-                        result = max(points, result);
-                    }
+class Solution
+{
+public:
+    long long maximumTripletValue(vector<int> &nums)
+    {
+        long long int result = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = i + 1; j < n; ++j)
+            {
+                for (int k = j + 1; k < n; ++k)
+                {
+                    long long int points = (long long)(nums[i] - nums[j]) * nums[k];
+                    result = max(points, result);
                 }
             }
-            if(result > 0) {
-                return result;
-            }
-            else {
-                return 0;
-            }
-    
-    
         }
-    };
+        if (result > 0)
+        {
+            return result;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+};
