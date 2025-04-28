dc01b107370447ba0edc74c6943c861b1afe3b4c
cmd-anurag <anuragkumar.7157@gmail.com>
2025-04-28 22:45:50 +0530

    268

---
+++ april/268.cpp
@@ -0,0 +1,22 @@
+#include<vector>
+#include<algorithm>
+using std::vector;
+
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        int expectedSum = n * (n+1)/2;
+
+        int actualSum = 0;
+
+        for(int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+};
