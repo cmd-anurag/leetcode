e9a5e20e3a10775739352b72f50af3faaf3c1971
cmd-anurag <anuragkumar.7157@gmail.com>
2025-06-02 22:32:06 +0530

    135

---
+++ june/135.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> candies(n, 1);
+
+        for (int i = 1; i < n; ++i)
+            if (ratings[i] > ratings[i - 1])
+                candies[i] = candies[i - 1] + 1;
+
+        for (int i = n - 2; i >= 0; --i)
+            if (ratings[i] > ratings[i + 1])
+                candies[i] = std::max(candies[i], candies[i + 1] + 1);
+
+        int total = 0;
+        for (int c : candies)
+            total += c;
+
+        return total;
+    }
+};
