5aeedb889d517154614652dbcc01dcee3a7d2549
Anurag Kumar <anuragkumar.7157@gmail.com>
2025-11-08 22:51:29 +0530

    1611

---
+++ november25/1611.cpp
@@ -0,0 +1,22 @@
+#include<cmath>
+using std::pow;
+
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        if(n == 0) {
+            return 0;
+        }
+
+        int k = 0;
+        while(pow(2, k) <= n) {
+            k++;
+        }
+
+        k--;
+
+        return pow(2, k+1) - 1 - minimumOneBitOperations((int)pow(2, k) ^ n);
+
+    }
+};
+// evil
